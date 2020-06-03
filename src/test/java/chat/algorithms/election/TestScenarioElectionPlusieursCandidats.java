c1ed9d1878670a8689a85f371edfa8988128674a
Bastien Sun <bastien.sun@protonmail.com>
2020-06-03 16:39:47 +0200

    test(election): start the candidature of s1 before s5 in test
    resolve #9

--- src/test/java/chat/algorithms/election/TestScenarioElectionPlusieursCandidats.java
+++ src/test/java/chat/algorithms/election/TestScenarioElectionPlusieursCandidats.java
@@ -53,8 +53,8 @@ public class TestScenarioElectionPlusieursCandidats extends Scenario {
         if (LOG_ON && TEST.isInfoEnabled()) {
             TEST.info("starting the test of the algorithms...");
         }
-        emulateAnInputLineFromTheConsoleForAServer(s5, "election");
         emulateAnInputLineFromTheConsoleForAServer(s1, "election");
+        emulateAnInputLineFromTheConsoleForAServer(s5, "election");
         if (LOG_ON && TEST.isInfoEnabled()) {
             TEST.info("end of the scenario.");
         }
